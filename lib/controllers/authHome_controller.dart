dedd8a7677c8b6d4e3bb6472543abf33e9128230
P-yiush07 <piyush062006@outlook.com>
2025-01-11 19:41:29 +0530

    Little more fixes

--- lib/controllers/authHome_controller.dart
+++ lib/controllers/authHome_controller.dart
@@ -33,14 +33,21 @@ class AuthController extends ChangeNotifier {
     Navigator.push(
       context,
       PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) => const SignInScreen(),
+        pageBuilder: (context, animation, secondaryAnimation) => 
+          const SignInScreen(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          return FadeTransition(
-            opacity: animation,
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.easeInOut;
+          var tween = Tween(begin: begin, end: end)
+              .chain(CurveTween(curve: curve));
+          var offsetAnimation = animation.drive(tween);
+          return SlideTransition(
+            position: offsetAnimation,
             child: child,
           );
         },
-        transitionDuration: const Duration(milliseconds: 200),
+        transitionDuration: const Duration(milliseconds: 300),
       ),
     );
   }
--- lib/views/sign_in_screen.dart
+++ lib/views/sign_in_screen.dart
@@ -45,25 +45,7 @@ class SignInView extends StatelessWidget {
               child: IconButton(
                 icon: const Icon(Icons.arrow_back, color: Colors.black),
                 onPressed: () {
-                  Navigator.of(context).pushReplacement(
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) => 
-                        const AuthHomeScreen(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                        const begin = Offset(-1.0, 0.0);
-                        const end = Offset.zero;
-                        const curve = Curves.easeInOut;
-                        var tween = Tween(begin: begin, end: end)
-                            .chain(CurveTween(curve: curve));
-                        var offsetAnimation = animation.drive(tween);
-                        return SlideTransition(
-                          position: offsetAnimation,
-                          child: child,
-                        );
-                      },
-                      transitionDuration: const Duration(milliseconds: 300),
-                    ),
-                  );
+                  Navigator.of(context).pop();
                 },
               ),
             ),
